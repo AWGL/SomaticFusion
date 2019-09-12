5a3443f41d07ae57ad4bc154981a1056e6083afc
Chris Medway <christophermedway@gmail.com>
2019-09-12 13:47:44 +0100

    added FASTQ processing code and config files

---
+++ 1_SomaticFusion.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# set -euo pipefail
+
+#PBS -l walltime=12:00:00
+#PBS -l ncpus=12
+#PBS_O_WORKDIR=(`echo $PBS_O_WORKDIR | sed "s/^\/state\/partition1//" `)
+#cd $PBS_O_WORKDIR
+
+# Description: Wrapper script for calling fusion genes in RNA-seq data \
+# from clinical samples using STAR-Fusion.
+# Arthor: Christopher Medway <christopher.medway@wales.nhs.uk>
+# Date: 6th June 2019
+# Useage: qsub run_star-fusion.sh [inside sample dir with .variables and \
+# .fastq.gz files]
+
+version=0.0.1
+
+. *.variables
+. /data/diagnostics/pipelines/SomaticFusion/SomaticFusion-$version/$panel/$panel.variables
+
+# set conda env
+source "$conda_bin_path"/activate SomaticFusion
+
+
+#######################
+# Preprocessing FASTQ #
+#######################
+
+#count how many core FASTQC tests failed
+countQCFlagFails() {
+    grep -E "Basic Statistics|Per base sequence quality|Per tile sequence quality|Per sequence quality scores|Per base N content" "$1" | \
+    grep -v ^PASS | \
+    grep -v ^WARN | \
+    wc -l | \
+    sed 's/^[[:space:]]*//g'
+}
+
+#record FASTQC pass/fail
+rawSequenceQuality=PASS
+
+#convert FASTQ to uBAM & add RGIDs
+for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq); do
+
+    #parse fastq filenames
+    laneId=$(echo "$fastqPair" | cut -d_ -f3)
+    read1Fastq=$(ls "$fastqPair"_R1_*fastq.gz)
+    read2Fastq=$(ls "$fastqPair"_R2_*fastq.gz)
+
+    #trim adapters
+    cutadapt \
+    -a "$read1Adapter" \
+    -A "$read2Adapter" \
+    -m 35 \
+    -o "$seqId"_"$sampleId"_"$laneId"_R1.fastq \
+    -p "$seqId"_"$sampleId"_"$laneId"_R2.fastq \
+    "$read1Fastq" \
+    "$read2Fastq"
+
+    #fastqc
+    fastqc -d /state/partition1/tmpdir --threads 12 --extract "$seqId"_"$sampleId"_"$laneId"_R1.fastq
+    fastqc -d /state/partition1/tmpdir --threads 12 --extract "$seqId"_"$sampleId"_"$laneId"_R2.fastq
+    mv "$seqId"_"$sampleId"_"$laneId"_R1_fastqc/summary.txt "$seqId"_"$sampleId"_"$laneId"_R1_fastqc.txt
+    mv "$seqId"_"$sampleId"_"$laneId"_R2_fastqc/summary.txt "$seqId"_"$sampleId"_"$laneId"_R2_fastqc.txt
+
+    #check FASTQC output
+    if [ $(countQCFlagFails "$seqId"_"$sampleId"_"$laneId"_R1_fastqc.txt) -gt 0 ] || [ $(countQCFlagFails "$seqId"_"$sampleId"_"$laneId"_R2_fastqc.txt) -gt 0 ]; then
+        rawSequenceQuality=FAIL
+    fi
+
+
+###################
+# Run STAR-Fusion #
+###################
+
+## STAR-fusion required a samples_file in order to handle multi-lane runs
+R1=$(for i in ./"$sampleId"_*.fastq.gz; do echo $i | grep "R1"; done)
+R2=$(for i in ./"$sampleId"_*.fastq.gz; do echo $i | grep "R2"; done)
+SAMPLE=$(for i in $R1;do echo $sampleId;done)
+paste <(printf %s "$SAMPLE") <(printf %s "$R1") <(printf %s "$R2") > "$sampleId".samples
+
+# run STAR-Fusion
+STAR-Fusion --genome_lib_dir $starfusion_lib \
+            --samples_file $sampleId.samples \
+            --output_dir ./STAR-Fusion/ \
+            --FusionInspector validate \
+            --denovo_reconstruct \
+            --examine_coding_effect \
+            --CPU $ncpus \
+            --min_FFPM 1
+
+
+###########################
+# Generate Fusion Reports #
+###########################
+
+# deactivate conda env
+source "$conda_bin_path"/deactivate
--- make-fusion-report.py
+++ make-fusion-report.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+make-fusion-report.py
+
+reformats / aggregates STAR-Fusion output \
+generates a single text report and and IGV batch file for each sample
+
+Aurthor: Christopher Medway
+Created: 12th September 2019
+Version: 0.0.1
+"""
+
 import os
 import argparse
 
-def get_args():
 
+def get_args():
+    """
+    parses command line arguments
+    """
     parser = argparse.ArgumentParser(
         description='generates a single report from STAR-fusion output files'
     )
@@ -15,22 +29,27 @@ def get_args():
     parser.add_argument('--panel', help='name of panel', required=True)
     parser.add_argument('--ip', help='required for igv batchfile', required=True)
 
-    args=parser.parse_args()
-    return(args)
+    args = parser.parse_args()
+    return (args)
+
 
 def make_igv_batchfile(args):
-
+    """
+    generates a batch file that can be uploaded into IGV
+    :param args:
+    :return:
+    """
     outputfile = "./fusionReport/" + args.sampleId + "_igv_report.batch"
 
-    path_for_igv = "//" + \
-	args.ip + \
-	"/results/" + \
-	args.seqId + \
-	"/" + \
-	args.panel + \
-	"/" + \
-	args.sampleId + \
-	"/STAR-Fusion/FusionInspector-validate/"
+    path_for_igv = '//' + \
+                   args.ip + \
+                   '/results/' + \
+                   args.seqId + \
+                   '/' + \
+                   args.panel + \
+                   '/' + \
+                   args.sampleId + \
+                   '/STAR-Fusion/FusionInspector-validate/'
 
     print(path_for_igv)
 
@@ -52,11 +71,12 @@ def make_igv_batchfile(args):
     out.close()
 
 
-
-
-
 def fusion_report(args):
-
+    """
+    compiles a text report from STAR-Fusion input files
+    :param args:
+    :return:
+    """
     outputfile = "./fusionReport/" + args.sampleId + "_fusionReport.txt"
     star_fusion_results_path = "./STAR-Fusion/FusionInspector-validate/finspector.FusionInspector.fusions.abridged.tsv.annotated.coding_effect"
 
@@ -89,15 +109,15 @@ def fusion_report(args):
             large_anchor_support = sfln[8]
             junc_read_ct = int(sfln[1])
             span_frag_ct = int(sfln[2])
-        
-            if large_anchor_support != 'NO_LDAS' and junc_read_ct > 0 and span_frag_ct > 0 and (junc_read_ct + span_frag_ct) > 2:
 
+            if large_anchor_support != 'NO_LDAS' and junc_read_ct > 0 and span_frag_ct > 0 and (
+                    junc_read_ct + span_frag_ct) > 2:
                 fusion = sfln[0]
                 left_breakpoint = sfln[5]
                 right_breakpoint = sfln[7]
                 num_counter_fusion_left = int(sfln[11])
                 num_counter_fusion_right = int(sfln[12])
-                        
+
                 # caculate the fusion alleleic ratio (FAR)
                 fafL = (junc_read_ct + span_frag_ct) / (num_counter_fusion_left + junc_read_ct + span_frag_ct)
                 fafR = (junc_read_ct + span_frag_ct) / (num_counter_fusion_right + junc_read_ct + span_frag_ct)
@@ -116,13 +136,13 @@ def fusion_report(args):
                     sfln[25] + "\t" +
                     str(num_counter_fusion_left) + "\t" +
                     str(num_counter_fusion_right) + "\t" +
-                    str(round(faf,2)) + "\n"
+                    str(round(faf, 2)) + "\n"
                 )
 
         out.close()
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     os.mkdir("./fusionReport")
 
     args = get_args()
