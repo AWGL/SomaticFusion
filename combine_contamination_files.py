67de0127ae23be6de1ee9fb3d864ae284ba5919f
Laura <laura.mccluskey@wales.nhs.uk>
2020-10-13 11:09:28 +0100

    Included RMATS contamination in combined file

--- combine_contamination_files.py
+++ combine_contamination_files.py
@@ -15,22 +15,23 @@ import pandas
 
 #read in star-fusion and arriba contamination files and merge them into one dataframe
 arriba = pandas.read_csv("contamination_arriba.csv", sep=",")
-star_fusion = pandas.read_csv("contamination_STAR_Fusion.csv", sep=",")
-overall = pandas.merge(arriba, star_fusion, on=["Sample"])
-
+star_fusion = pandas.read_csv("contamination_star_fusion.csv", sep=",")
+RMATS= pandas.read_csv("contamination_RMATS.csv", sep=",")
+starFusion_arriba = pandas.merge(arriba, star_fusion, on=["Sample"])
+overall = pandas.merge(starFusion_arriba, RMATS, on=["Sample"])
 
 #function to combine the star-fusion and arriba contamination columns
 def contamination(table):
-    if ((table["Contamination_arriba"]=="Yes") or (table["Contamination_STAR_Fusion"]=="Yes")):
+    if ((table["Contamination_arriba"]=="Yes") or (table["Contamination_star_fusion"]=="Yes")):
         return "Yes"
     else:
         return "No"
 
 
 
-#combine the arriba and star-fusion referral contamination files
+#combine the arriba and star-fusion and RMATS referral contamination files
 def contamination_referral(table):
-    if ((table["Contamination_referral_arriba"]=="Yes") or (table["Contamination_referral_STAR_Fusion"]=="Yes")):
+    if ((table["Contamination_referral_arriba"]=="Yes") or (table["Contamination_referral_star_fusion"]=="Yes") or (table["Contamination_referral_RMATS"]=="Yes")):
         return "Yes"
     else:
         return "No"
@@ -39,6 +40,6 @@ def contamination_referral(table):
 
 overall["contamination"]= overall.apply(lambda overall : contamination(overall), axis=1)
 overall["contamination_referral"]= overall.apply(lambda overall : contamination_referral(overall), axis=1)
-del [overall["Contamination_arriba"],overall["Contamination_referral_arriba"], overall["Contamination_STAR_Fusion"], overall["Contamination_referral_STAR_Fusion"]]
+del [overall["Contamination_arriba"],overall["Contamination_referral_arriba"], overall["Contamination_star_fusion"], overall["Contamination_referral_star_fusion"], overall["Contamination_referral_RMATS"]]
 
 overall.to_csv('contamination.csv', index=False)
