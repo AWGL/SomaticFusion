81f15943d7be840e989ede341c43ecfbae88ff6f
Laura <laura.mccluskey@wales.nhs.uk>
2020-10-13 14:00:45 +0100

    Change contamination.csv to Contamination.csv to fit with autoqc database

--- combine_contamination_files.py
+++ combine_contamination_files.py
@@ -22,7 +22,7 @@ overall = pandas.merge(starFusion_arriba, RMATS, on=["Sample"])
 
 #function to combine the star-fusion and arriba contamination columns
 def contamination(table):
-    if ((table["Contamination_arriba"]=="Yes") or (table["Contamination_star_fusion"]=="Yes")):
+    if ((table["Contamination_arriba"]=="Yes") or (table["Contamination_star_fusion"]=="Yes") or (table["Contamination_RMATS"]=="Yes")):
         return "Yes"
     else:
         return "No"
@@ -38,8 +38,8 @@ def contamination_referral(table):
 
 
 
-overall["contamination"]= overall.apply(lambda overall : contamination(overall), axis=1)
-overall["contamination_referral"]= overall.apply(lambda overall : contamination_referral(overall), axis=1)
-del [overall["Contamination_arriba"],overall["Contamination_referral_arriba"], overall["Contamination_star_fusion"], overall["Contamination_referral_star_fusion"], overall["Contamination_referral_RMATS"]]
+overall["Contamination"]= overall.apply(lambda overall : contamination(overall), axis=1)
+overall["Contamination_referral"]= overall.apply(lambda overall : contamination_referral(overall), axis=1)
+del [overall["Contamination_arriba"],overall["Contamination_referral_arriba"], overall["Contamination_star_fusion"], overall["Contamination_referral_star_fusion"], overall["Contamination_referral_RMATS"], overall["Contamination_RMATS"]]
 
 overall.to_csv('contamination.csv', index=False)
