06fee46a224551ef6c31440da95c47d95d9e6ce3
Laura McCluskey <laura.mccluskey@wales.nhs.uk>
2021-01-07 13:42:20 +0000

    update version

--- 1_SomaticFusion.sh
+++ 1_SomaticFusion.sh
@@ -13,7 +13,7 @@ cd $PBS_O_WORKDIR
 # Usage: qsub run_star-fusion.sh [inside sample dir with .variables and \
 # .fastq.gz files]
 
-version="1.0.6"
+version="1.0.7"
 
 # source variables file
 . *.variables
